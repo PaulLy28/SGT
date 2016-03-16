8bd5fad527d199ac840f4e7021c2479ea79d8e69
Paul Ly <28ly.paul@gmail.com>
2016-03-15 22:33:41 -0700

    complete

--- firebase_alt.js
+++ firebase_alt.js
@@ -1,15 +1,27 @@
-var app = angular.module("sgtApp", ["firebase"])
+var app = angular.module('firebaseSGT', ['firebase']);
 
-    .service("studentService", function($firebaseArray){
-        var self = this;
-        this.ref = new Firebase("https://studenttable.firebaseio.com/students");
-        this.array = {};
+app.service('firebaseService', function(){
+    var self = this;
+    //this.firebaseRef = new Firebase('https://lfchallenge.firebaseio.com/students');
+    this.firebaseRef = new Firebase('https://studenttable.firebaseio.com/students');
+    this.student = {};
+    this.addStudent = function(){
+        self.firebaseRef.push(this.student);
+        this.student = {};
+    };
+    this.deleteStudent = function(key){
+        self.firebaseRef.child(key).remove();
+    };
+    this.editStudent = function(key){
+        var edit = self.firebaseRef.child(key);
+        edit.update(this.student);
+        this.student = {};
+    }
+});
 
-        this.addStudent = function(){
-            this.array.push(key);
-
-        }
-
-    })
-
-    .controller("mainController", function())
+app.controller('MainController', function($firebaseObject, firebaseService){
+    this.addStudent = firebaseService.addStudent;
+    this.databaseObj = $firebaseObject(firebaseService.firebaseRef);
+    this.deleteStudent = firebaseService.deleteStudent;
+    this.editStudent = firebaseService.editStudent;
+});
