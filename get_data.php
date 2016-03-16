6831e26339ae0dd489dad537ffc80a9fa47402c9
kkennethlee <kyung.kenneth.lee@gmail.com>
2016-03-16 15:52:04 -0700

    back on track

--- get_data.php
+++ get_data.php
@@ -2,7 +2,7 @@
     require('mysql_connect.php');
     //this query string returns all active students
     //$studentQuery = "SELECT studenttable.name, studenttable.course, studenttable.grade, studenttable.Student_ID WHERE studenttable.Status = 1";
-    $query = "SELECT studenttable.name, studenttable.course, studenttable.grade, studenttable.Student_ID,
+    $query = "SELECT studenttable.name, studenttable.course, studenttable.grade, studenttable.Student_ID AS 'id',
         teachertable.Name AS 'Teacher Name', studenttable.Teacher_ID FROM studenttable, teachertable WHERE studenttable.Status = 1";
     $result = mysqli_query($connect, $query) or die("Error in Selecting " . mysqli_error($connect));
     //print_r($result);
