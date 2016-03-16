34b0bdad7664d9aa124d8f61e6bfddce431281dd
Paul Ly <28ly.paul@gmail.com>
2016-03-15 22:54:17 -0700

    cleaned up code

--- firebase.js
+++ firebase.js
@@ -1,22 +1,4 @@
 var app = angular.module("sgtApp", ["firebase"])
-/*controller only*/
-/*    .controller("mainController", function($scope, $firebaseArray){
-        this.test = "test string";
-        var ref = new Firebase("https://popping-heat-5383.firebaseio.com/students");
-        this.studentArray = $firebaseArray(ref);
-
-        this.addStudent = function(){
-            this.studentArray.$add({
-            name: this.studentName,
-            course: this.course,
-            grade: this.studentGrade
-            })
-        }
-        this.removeStudent = function(index){
-            this.studentArray.$remove(index);
-        }
-    })*/
-
 /*service and controller*/
     .service("studentService", function($firebaseArray){
         var ref = new Firebase("https://studenttable.firebaseio.com/students");
@@ -34,30 +16,17 @@ var app = angular.module("sgtApp", ["firebase"])
                         callback();
                     }
                 )
-
         },
         this.removeStudent = function(index){
             this.array.$remove(index);
         }
-
-
     })
 
     .controller("mainController", function($scope, $firebaseArray, studentService){
-      /*  var mainCtrlScope = this;*/
+        var mainCtrlScope = this;
         this.studentArray = studentService.array;
-        this.studentObj = {
-/*            name: this.studentName,
-            course: this.course,
-            grade: this.studentGrade*/
-        };
+        this.studentObj = {};
         this.addNewStudent = function(){
-/*
-            var studentObj = {
-                name: this.studentName,
-                course: this.course,
-                grade: this.studentGrade
-            };*/
             studentService.addStudent(this.studentObj, this.clearInputs);
             /*studentService.clearInputs();*/
         };
@@ -65,9 +34,84 @@ var app = angular.module("sgtApp", ["firebase"])
              studentService.removeStudent(index);
         };
             this.clearInputs = function() {
-                this.studentObj = {};
-/*                mainCtrlScope.studentName = "",
-                mainCtrlScope.course = "",
-                mainCtrlScope.studentGrade = ""*/
+                mainCtrlScope.studentObj = {};
         }
     });
+
+
+
+
+
+/*controller only*/
+/*    .controller("mainController", function($scope, $firebaseArray){
+ this.test = "test string";
+ var ref = new Firebase("https://popping-heat-5383.firebaseio.com/students");
+ this.studentArray = $firebaseArray(ref);
+
+ this.addStudent = function(){
+ this.studentArray.$add({
+ name: this.studentName,
+ course: this.course,
+ grade: this.studentGrade
+ })
+ }
+ this.removeStudent = function(index){
+ this.studentArray.$remove(index);
+ }
+ })*/
+
+/*    /!*service and controller*!/
+ .service("studentService", function($firebaseArray){
+ var ref = new Firebase("https://studenttable.firebaseio.com/students");
+ this.array = $firebaseArray(ref);
+
+ this.addStudent = function(obj, callback){
+ console.log("object", obj);
+ this.array.$add({
+ name: obj.name,
+ course: obj.course,
+ grade: obj.grade
+ })
+ .then(
+ function(){
+ callback();
+ }
+ )
+
+ },
+ this.removeStudent = function(index){
+ this.array.$remove(index);
+ }
+
+
+ })
+
+ .controller("mainController", function($scope, $firebaseArray, studentService){
+ /!*  var mainCtrlScope = this;*!/
+ this.studentArray = studentService.array;
+ this.studentObj = {
+ /!*            name: this.studentName,
+ course: this.course,
+ grade: this.studentGrade*!/
+ };
+ this.addNewStudent = function(){
+ /!*
+ var studentObj = {
+ name: this.studentName,
+ course: this.course,
+ grade: this.studentGrade
+ };*!/
+ studentService.addStudent(this.studentObj, this.clearInputs);
+ /!*studentService.clearInputs();*!/
+ };
+ this.removeStudentServer = function(index){
+ studentService.removeStudent(index);
+ };
+ this.clearInputs = function() {
+ this.studentObj = {};
+ /!*                mainCtrlScope.studentName = "",
+ mainCtrlScope.course = "",
+ mainCtrlScope.studentGrade = ""*!/
+ }
+ }); */
+
